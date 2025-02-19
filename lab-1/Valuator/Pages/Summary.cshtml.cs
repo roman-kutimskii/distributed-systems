0337228e40e1185c5c6cdf317226134eb0789e18
Roman Kutimskii <roman.kutimskii@gmail.com>
2025-02-19 21:54:46 +0300

    Реализовать TODO на странице Summary.cshtml.cs

--- lab-1/Valuator/Pages/Summary.cshtml.cs
+++ lab-1/Valuator/Pages/Summary.cshtml.cs
@@ -5,15 +5,18 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
 
 namespace Valuator.Pages;
 public class SummaryModel : PageModel
 {
     private readonly ILogger<SummaryModel> _logger;
+    private readonly IDatabase _redisDb;
 
-    public SummaryModel(ILogger<SummaryModel> logger)
+    public SummaryModel(ILogger<SummaryModel> logger, IConnectionMultiplexer redis)
     {
         _logger = logger;
+        _redisDb = redis.GetDatabase();
     }
 
     public double Rank { get; set; }
@@ -22,7 +25,19 @@ public class SummaryModel : PageModel
     public void OnGet(string id)
     {
         _logger.LogDebug(id);
+        
+        string rankKey = "RANK-" + id;
+        var rankValue = _redisDb.StringGet(rankKey);
+        if (rankValue.HasValue && double.TryParse(rankValue, out double rank))
+        {
+            Rank = rank;
+        }
 
-        // TODO: (pa1) проинициализировать свойства Rank и Similarity значениями из БД (Redis)
+        string similarityKey = "SIMILARITY-" + id;
+        var similarityValue = _redisDb.StringGet(similarityKey);
+        if (similarityValue.HasValue && double.TryParse(similarityValue, out double similarity))
+        {
+            Similarity = similarity;
+        }
     }
 }
