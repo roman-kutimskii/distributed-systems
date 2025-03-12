c323e9c5fb2056f3a364e5d9db0c8fbaffc98333
Roman Kutimskii <roman.kutimskii@gmail.com>
2025-03-12 22:34:43 +0300

    Добавить проект второй лабораторной

---
+++ lab-2/Valuator/Program.cs
@@ -0,0 +1,35 @@
+using StackExchange.Redis;
+
+namespace Valuator;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+        
+        var redisConnectionString = builder.Configuration.GetConnectionString("Redis");
+        var redis = ConnectionMultiplexer.Connect(redisConnectionString);
+        builder.Services.AddSingleton<IConnectionMultiplexer>(redis);
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+        }
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapRazorPages();
+
+        app.Run();
+    }
+}
