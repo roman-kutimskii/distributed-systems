21ea66947a1fca16a0c0c2f1cff5745b4cbcd6a2
Roman Kutimskii <roman.kutimskii@gmail.com>
2025-05-21 19:38:15 +0300

    Переименование проекта ValuatorSpecs в Valuator.Specs и добавление TestServerFixture для интеграционного тестирования.

---
+++ lab-7/tests/Valuator.Specs/Fixture/TestServerFixture.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Valuator.Specs.Fixture;
+
+public class TestServerFixture
+{
+    public HttpClient HttpClient { get; }
+
+    public TestServerFixture()
+    {
+        WebApplicationFactory<Program> factory = new();
+        HttpClient = factory.CreateClient();
+    }
+}
