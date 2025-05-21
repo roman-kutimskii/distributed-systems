f24c480d4861bb9c23df3d6b27048d53b6a0b540
Roman Kutimskii <roman.kutimskii@gmail.com>
2025-05-21 20:13:53 +0300

    Добавлен CustomWebApplicationFactory для настройки тестового окружения.

---
+++ lab-7/tests/Valuator.Specs/Fixture/CustomWebApplicationFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Valuator.Specs.Fixture;
+
+public class CustomWebApplicationFactory<TEntryPoint>(string dbConnectionString)
+    : WebApplicationFactory<TEntryPoint> where TEntryPoint : class
+{
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.ConfigureHostConfiguration(configurationBuilder =>
+        {
+            configurationBuilder.AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                { "ConnectionStrings:MainConnection", dbConnectionString }
+            });
+        });
+
+        return base.CreateHost(builder);
+    }
+}
